ddf2e85411218d26170bd8f64219756af50943d4
ulyildiz <ulashjob@gmail.com>
2024-04-10 00:40:08 +0300

    ilk_kalıp

--- main.c
+++ main.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include "defines.h"
 
 int main (int ac, char** av, char **env)
 {
+    t_main  shell;
+
     if (ac >=2)
         return(printf("Wrong Argument"));
+    //if (initialize(&shell))
+    //    return (printf("Couldn't initialize"), 1);
+    //signal
+    //start_shell(&shell);
     ac = 0;
     while (env[ac])
     {
