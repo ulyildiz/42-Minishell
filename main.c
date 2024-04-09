4a3c3512949ca6d0cd3552342482d1cf96c6998d
Yunus Emre <yunusemresarac4152@gmail.com>
2024-04-10 00:27:38 +0300

    env yazdırıldı

--- main.c
+++ main.c
@@ -1,4 +1,14 @@
-int main ()
+#include <stdio.h>
+
+int main (int ac, char** av, char **env)
 {
+    if (ac >=2)
+        return(printf("Wrong Argument"));
+    ac = 0;
+    while (env[ac])
+    {
+        printf("%s\n",env[ac++]);
+    }
+    
     return (0);
 }
