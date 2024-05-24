b251839d2d8e2d5c05e7f07e9e332db3ba8bb6cb
Yunus Emre SARAÇ <troubledkezoo1@Yunus-MacBook-Pro.local>
2024-05-24 21:12:00 +0300

    sonunda algoritmaya uygun pipe ve dup2 kullanıp dinamik | kullanılabilir shell altyapısı tamamland free işlemleri ve hata düzeltmeleri kaldi

--- executor.c
+++ executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ulyildiz <ulyildiz@student.42kocaeli.com.t +#+  +:+       +#+        */
+/*   By: ulyildiz <ulyildiz@student.42kocaeli.com.t>+#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 11:33:54 by ulyildiz          #+#    #+#             */
-/*   Updated: 2024/05/23 11:33:54 by ulyildiz         ###   ########.fr       */
+/*   Updated: 2024/05/24 21:08:54 by ysarac	          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,10 @@
 #include "42-libft/libft.h"
 #include <stdio.h>
 #include <unistd.h>
-
 #include <sys/types.h>
 #include <sys/wait.h>
 
-static void official_executer(t_command *cmds, t_main *shell, int i)
+static void official_executer(t_command *cmds, t_main *shell, int i, int in_fd, int out_fd)
 {
 	cmds->pid = fork();
 	if (cmds->pid == -1)
@@ -28,43 +27,70 @@ static void official_executer(t_command *cmds, t_main *shell, int i)
 	}
 	else if (cmds->pid == 0)
 	{
-		close(cmds->pipefd[i % 2]);
-		dup2(cmds->pipefd [ ( i % 2 ) ^ 1 ] , STDOUT_FILENO );
-		close(cmds->pipefd[ ( i % 2 ) ^ 1 ] );
-		execve(cmds->cmd_and_path, cmds->value, shell->env_for_execve_function); // Execute ls -la
-        perror("exec ls");
-        exit(EXIT_FAILURE);
+		if (in_fd != STDIN_FILENO)
+		{
+			dup2(in_fd, STDIN_FILENO);
+			close(in_fd);
+		}
+		if (out_fd != STDOUT_FILENO)
+		{
+			dup2(out_fd, STDOUT_FILENO);
+			close(out_fd);
+		}
+		execve(cmds->cmd_and_path, cmds->value, shell->env_for_execve_function);
+		perror("execve");
+		exit(EXIT_FAILURE);
 	}
-	
-	
-
 }
 
-
-int	executor(t_main *shell)
+int executor(t_main *shell)
 {
-	int i;
-	i = 0;
-	t_command *tmp = shell->cmd;
-	t_command *cmds;
+	int i = 0;
+	int in_fd = STDIN_FILENO;
+	int pipefd[2];
+	t_command *cmds = shell->cmd;
 
-	cmds = shell->cmd;
-	while(cmds)
+	while (cmds)
 	{
-		if (pipe(cmds->pipefd) == -1)
+		if (cmds->next)
 		{
-			perror("pipe");
-			exit(EXIT_FAILURE);
+			if (pipe(pipefd) == -1)
+			{
+				perror("pipe");
+				exit(EXIT_FAILURE);
+			}
 		}
-		if (is_builtin(cmds, shell))
-			;
-		else if (accessibility(cmds,shell))
-			official_executer(cmds, shell, i);
 		else
-			return (ft_putstr_fd("ft_sh: command not found: ", 2),
-				ft_putstr_fd(cmds->value[0], 2), ft_putchar_fd('\n', 2), 2);
+		{
+			pipefd[1] = STDOUT_FILENO;
+		}
+
+		if (is_builtin(cmds, shell))
+		{
+			// Handle built-in command
+			// Implement your built-in command handling logic here
+		}
+		else if (accessibility(cmds, shell))
+		{
+			official_executer(cmds, shell, i, in_fd, pipefd[1]);
+		}
+		else
+		{
+			ft_putstr_fd("ft_sh: command not found: ", 2);
+			ft_putstr_fd(cmds->value[0], 2);
+			ft_putchar_fd('\n', 2);
+			return 2;
+		}
+		if (pipefd[1] != STDOUT_FILENO)
+			close(pipefd[1]);
+		if (in_fd != STDIN_FILENO)
+			close(in_fd);
+		in_fd = pipefd[0];
+
+		waitpid(cmds->pid, NULL, 0);
 		i++;
 		cmds = cmds->next;
 	}
-	return (1);
+
+	return 1;
 }
