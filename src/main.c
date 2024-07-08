f593da5c14db346d3a56ecf0029c781b3be12c52
Ulas Berke Yildiz <ulyildiz@k2m05s03.42kocaeli.com.tr>
2024-07-08 15:56:16 +0300

    isatty

--- src/main.c
+++ src/main.c
@@ -25,14 +25,19 @@ int	main(int ac, char **av, char **env)
 {
 	t_main	shell;
 
-	signal_reciever(1);
-	disable_echo_control_chars();
-	av = (void *)av;
-	if (ac >= 2)
-		return (ft_putendl_fd("Too much argument.", 2), 1);
-	if (!initialize(&shell, env))
-		return (perror("Initialize"), 1);
-	shell_keeper(&shell);
-	start_shell(&shell);
+	if (isatty(STDIN_FILENO) && isatty(STDOUT_FILENO))
+	{
+		signal_reciever(1);
+		disable_echo_control_chars();
+		av = (void *)av;
+		if (ac >= 2)
+			return (ft_putendl_fd("Too much argument.", 2), 1);
+		if (!initialize(&shell, env))
+			return (perror("Initialize"), 1);
+		shell_keeper(&shell);
+		start_shell(&shell);
+	}
+	else
+		perror("ft_sh:");
 	return (shell.exit_status);
 }
